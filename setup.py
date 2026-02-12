2dafc8d4ddc12aac3e794107d0d6eb15d0aeae24
Patrick Jiang <patjiang04@gmail.com>
2026-02-12 11:02:06 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,8 @@ def get_compiler_type():
     cxx = os.environ.get('CXX', 'g++')
     try:
         out = subprocess.check_output([cxx, '--version'], stderr=subprocess.STDOUT).decode()
-        if 'icpx' in out or 'oneAPI' in out or 'Intel' in out:
+        if any(x in out for x in ['icpx', 'oneAPI', 'Intel']):
             return 'intel'
-        if 'clang' in out:
-            return 'clang'
     except Exception:
         pass
     return 'gcc'
@@ -28,9 +26,7 @@ def get_ext_modules():
     if compiler_type == 'intel':
         nvcc_flags += ['--allow-unsupported-compiler']
         cxx_flags += ['-fp-model=precise']
-    elif compiler_type == 'gcc':
-        cxx_flags += ['-Wno-deprecated-declarations']
-
+    
     force_build = os.environ.get('FORCE_CUDA_BUILD', '0') == '1'
 
     if (torch.cuda.is_available() and CUDA_HOME is not None) or force_build:
@@ -41,38 +37,15 @@ def get_ext_modules():
                     'src/real_spherical_harmonics/rsh_bind.cpp',
                     'src/real_spherical_harmonics/rsh_cuda.cu'
                 ],
-                extra_compile_args={
-                    'cxx': cxx_flags,
-                    'nvcc': nvcc_flags
-                }
+                extra_compile_args={'cxx': cxx_flags, 'nvcc': nvcc_flags}
             )
         ]
-        cmdclass = {'build_ext': BuildExtension}
-    else:
-        ext_modules = None
-        cmdclass = {}
-
-    return ext_modules, cmdclass
+        return ext_modules, {'build_ext': BuildExtension}
+    return None, {}
 
 ext_modules, cmdclass = get_ext_modules()
 
 setup(
-    name='se3cnn',
-    url='https://github.com/mariogeiger/se3cnn',
-    author='Mario Geiger',
-    version='0.1.0',
-    python_requires='>=3.8',
-    install_requires=[
-        'scipy',
-        'appdirs',
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
     ext_modules=ext_modules,
     cmdclass=cmdclass,
-    packages=find_packages(),
-    zip_safe=False,
 )
